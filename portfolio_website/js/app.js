1f8f6438d80027602c79618e40755dc476721b1b
Brian Sachleben <bsachleben@gmail.com>
2017-03-15 13:41:27 -0400

    added images, JSON file, and functionality.  wrote app.js so that image gallery loads from JSON file and large image and description opens at top of page when you click on thumbnail.  Close button and Prev / Next buttons are functional on large image div.

--- portfolio_website/js/app.js
+++ portfolio_website/js/app.js
@@ -1,15 +1,114 @@
 //Problem: image gallery needs interactability when clicking on images
 //Solution: use jQuery to increase size of images from 50% screen width to 100% screen width (on mobile) when clicked
 $(document).ready(function() {
+  //initialize some variables
+  var $largeImage = $("#large_image");
+  var $galleryContainingDiv = document.getElementById('image_list');
+  var $imageList = $galleryContainingDiv.children;
+  var imageArray = [];//Holds info for #large_image div
+  var largeImagePlaceholder = document.getElementById('image_placeholder');
+  var titlePlaceholder = document.getElementById('image_title');
+  var descripPlaceholder = document.getElementById('image_description');
+  var imageArrayPosition = '';//saves the position within imageArray for use by prev / next buttons
+
+    // AJAX loading image data from JSON file
+
+    var url = "../data/images.json";
+      $.getJSON(url, function (response) {
+        //for each object in JSON file...
+        $.each(response, function (index, image) {
+
+          var imgName = image.name;
+          var imgDescrip = image.description;
+          var imgThumb = image.thumbURL;
+          var imgOrigin = image.enlargedURL;
+
+          var largeImageInfo = {
+            image : imgOrigin,
+            title : imgName,
+            description : imgDescrip
+          }
+          console.log(largeImageInfo);
+
+          var targetLocation = $('#image_list');
+          var containingDiv = document.createElement('div');
+          var link = document.createElement('a');
+          var img = document.createElement('img');
+
+          //Create thumbnail image in gallery (#image_list div)
+          $(containingDiv).addClass('image_sml');
+          $(containingDiv).addClass('col-xs-6');
+          $(containingDiv).addClass('col-sm-3');
+          $(containingDiv).addClass('col-lg-2');
+          $(link).attr('href', '#large_image');
+          $(img).attr('src', imgThumb);
+          $(img).attr('alt', imgName);
+
+          targetLocation.append(containingDiv);
+          containingDiv.append(link);
+          link.append(img);
+
+          //Populate imageArray with large images
+          imageArray.push(largeImageInfo);
+
+          //Bind click event to image
+          bindClickEvent(link);
+
+        });
+
+      });
+
+
+
+      // end AJAX loading image data from JSON file
+
   //when page loads and before anything is clicked
+
     //hide image with caption div
+    $largeImage.hide();
+  //When thumbnail version of image is clicked...
+    var openLargeImage = function() {
+      //create img element for large version of image
+      var largeImageElement = document.createElement('img');
+      var imageClicked = [].indexOf.call($imageList, this.parentNode) - 1;
+      $(largeImageElement).attr('src', imageArray[imageClicked].image);
+      imageArrayPosition = imageClicked;
+      //clear, then append image element created above to image_placeholder div
+      largeImagePlaceholder.innerHTML = '';
+      largeImagePlaceholder.append(largeImageElement);
+      //clear, then fill h2 id image_title with image title text
+      titlePlaceholder.innerText = '';
+      titlePlaceholder.innerText = imageArray[imageClicked].title;
+      //clear, then fill p id image_description with description text
+      descripPlaceholder.innerText = '';
+      descripPlaceholder.innerText = imageArray[imageClicked].description;
+      //show large_image div
+      $largeImage.show();
 
-  //When image is clicked
-    //remove active class from all sibling divs
-    //hide thumbnail image
-    //add active class to clicked image
-    //show image with caption div
+    }
 
+    var refreshLargeImage = function() {
+      //create img element for large version of image
+      var largeImageElement = document.createElement('img');
+      $(largeImageElement).attr('src', imageArray[imageArrayPosition].image);
+      //clear, then append image element created above to image_placeholder div
+      largeImagePlaceholder.innerHTML = '';
+      largeImagePlaceholder.append(largeImageElement);
+      //clear, then fill h2 id image_title with image title text
+      titlePlaceholder.innerText = '';
+      titlePlaceholder.innerText = imageArray[imageArrayPosition].title;
+      //clear, then fill p id image_description with description text
+      descripPlaceholder.innerText = '';
+      descripPlaceholder.innerText = imageArray[imageArrayPosition].description;
+      //show large_image div
+      $largeImage.show();
+    }
+
+    //bind click event to thumbnail images
+
+    var bindClickEvent = function(element) {
+      element.onclick = openLargeImage;
+    }
 
 
 
@@ -22,4 +121,51 @@ $(document).ready(function() {
     event.preventDefault();
     $menu.toggle();
   });
+
+  // Large Image section controls
+
+  var $closeImage = $(".close_image a");
+  var $prevButton = $(".prev_button");
+  var $nextButton = $(".next_button");
+
+  // close image
+  $closeImage.on("click", function(event) {
+    event.preventDefault();
+    console.log("close image...");
+    $largeImage.hide();
+  });
+
+  // previous image
+  $prevButton.on("click", function(event) {
+    event.preventDefault();
+    var arrayLength = imageArray.length - 1;
+    console.log("previous image...");
+    console.log(imageArray.length);
+    if (imageArrayPosition !== 0) {
+      imageArrayPosition --;
+    } else {
+      imageArrayPosition = arrayLength;
+    }
+    refreshLargeImage();
+  });
+
+  // next image
+  $nextButton.on("click", function(event) {
+    event.preventDefault();
+    var arrayLength = imageArray.length - 1;
+    console.log("next image...");
+    console.log(imageArray.length);
+    if (imageArrayPosition < arrayLength) {
+      imageArrayPosition ++;
+    } else {
+      imageArrayPosition = 0;
+    }
+    refreshLargeImage();
+  });
+
+  // end Large Image section controls
+
+//Future additions: limit initial load to 10-ish images and load more on document scroll with AJAX ($(window).scroll(function(){ code here });)
+//Future additions: add google maps to photographs, possibly using image latitude and longitude metadata
+
 }); /* end document.ready function */
